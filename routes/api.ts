84848e90e338004b028511ee787708f256b6a688
金井建斗 <macos@macosnoMBP.lan>
2023-07-12 21:59:39 +0900

    add:status code

--- routes/api.ts
+++ routes/api.ts
@@ -3,7 +3,19 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ message: "Hello" });
+  res.setHeader("X-Timestamp", Date.now());
+
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
+  if (message === "") {
+    res.status(400);
+    if (lang === "en") {
+      message = "message is empty";
+    } else {
+      message = "messageの値が空です";
+    }
+  }
+  res.send({ message });
 });
 
 router.use(express.json());
